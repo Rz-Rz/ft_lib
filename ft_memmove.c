8aee1f14a75e782c54f9ba3610596852f5819173
Karim DHRIF <kdhrif@e1r10p9.clusters.42paris.fr>
2022-05-05 15:29:37 +0200

    isalpha

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,19 +2,22 @@
 
  void *ft_memmove(void *dest, const void *src, size_t n)
 {
-		size_t i;                                
-                                  
+	size_t i;                                
+
+	if (!dest && !src)
+		return (NULL);
+
           i = 0;                                
           if (dest > src) {                                
-                  while(n) {                                
-                          dest[n] = src[n];                                
-                          n--;                                
+                  while(n) {
+			  ((unsigned char*)dest)[n] = ((unsigned char)src)[n];
+			  n--;                                
                   }                                
           }                                
           else {                                
                   while (i < n)                                
                   {                                
-                          dest[i] = src[i];                                
+                          ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
                           i++;                                
                   }                                
           }                                
