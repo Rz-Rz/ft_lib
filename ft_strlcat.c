f71bc7ab1929a25a35ecc75770a1045d065b027b
Karim DHRIF <kdhrif@e2r12p9.clusters.42paris.fr>
2022-05-10 15:32:35 +0200

    fixed strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: kdhrif <kdhrif@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 15:35:55 by kdhrif            #+#    #+#             */
-/*   Updated: 2022/05/09 15:37:32 by kdhrif           ###   ########.fr       */
+/*   Updated: 2022/05/10 15:32:17 by kdhrif           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	j;
 	size_t	srcln;
 
-	srcLn = ft_strlen(src);
+	srcln = ft_strlen(src);
 	i = 0;
 	while (i < size && dest[i])
 		i++;
 	if (i == size)
-		return (i + srcLn);
+		return (i + srcln);
 	j = -1;
 	while ((++j + i) < size - 1 && src[j])
 		dest[i + j] = src[j];
 	dest[j + i] = '\0';
-	return (i + srcLn);
+	return (i + srcln);
 }
