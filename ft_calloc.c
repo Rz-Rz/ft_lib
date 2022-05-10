d17c6abd714ac18f0b2de8aaf1e15042a5a0571c
Karim DHRIF <kdhrif@e2r12p9.clusters.42paris.fr>
2022-05-10 15:09:44 +0200

    calloc

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdhrif <kdhrif@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 14:49:10 by kdhrif            #+#    #+#             */
+/*   Updated: 2022/05/10 15:08:26 by kdhrif           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, nmemb);
+	return (ptr);
+}
