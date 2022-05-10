edb8008c55b93ebe4d396c8be8eec7d500004e34
Karim DHRIF <kdhrif@e2r12p9.clusters.42paris.fr>
2022-05-10 12:26:24 +0200

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdhrif <kdhrif@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 12:25:11 by kdhrif            #+#    #+#             */
+/*   Updated: 2022/05/10 12:25:59 by kdhrif           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (*little == NULL)
+		return (big);
+	i = 0;
+	while (big[i])
+	{
+		j = 0;
+		while (big[i + j] == little[j] && (i + j) < len)
+		{
+			if (big[i + j] == '\0' && little[j] == '\0')
+				return ((char *)&big[i + j]);
+					j++;
+		}
+		if (little[j] == '\0')
+			return ((char *)&big[i + j]);
+				i++;
+	}
+	return (NULL);
+}
