23201aaae3f0033b29b0c3a5b5b584e8542ad098
Karim DHRIF <kdhrif@e2r12p9.clusters.42paris.fr>
2022-05-10 11:55:56 +0200

    norm of the new funcs

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kdhrif <kdhrif@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/10 11:55:28 by kdhrif            #+#    #+#             */
-/*   Updated: 2022/05/10 11:55:29 by kdhrif           ###   ########.fr       */
+/*   Created: 2022/05/10 11:53:50 by kdhrif            #+#    #+#             */
+/*   Updated: 2022/05/10 11:53:56 by kdhrif           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strchr(const char *s, int c)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	i;
+	size_t	i;
 
-	i = -1;
-	while (s[++i])
-		if (s[i] == c)
-			return ((char *)s + i);
+	i = 0;
+	while (s1[i] == s2[i] && (s1[i] || s2[i]) && i < n - 1)
+		i++;
+	if (s1[i] != s2[i] && (n != 0))
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (0);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdhrif <kdhrif@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 11:55:12 by kdhrif            #+#    #+#             */
+/*   Updated: 2022/05/10 11:55:14 by kdhrif           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
- char *ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-	int size;
+	int	size;
 
 	++size = ft_strlen(s);
 	while (--size >= 0)
