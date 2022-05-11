34535c2ae51bef316bd528f7627987b1c2f68da2
Karim DHRIF <kdhrif@e2r10p18.clusters.42paris.fr>
2022-05-11 16:11:48 +0200

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,2 +1,17 @@
 #include "libft.h"
 
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	size_t i;
+	size_t j;
+	char *ptr;
+
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	ptr = malloc(sizeof(char) * (i+j));
+	if (!ptr)
+		return (NULL);
+	ptr = ft_memcpy(ptr, s1, i);
+	ft_strlcat(ptr, s2, (i+j));	
+	return (ptr);
+}
