f683c4e9d246923bd9e4c0de7c149767323f708d
Karim DHRIF <kdhrif@e2r10p19.clusters.42paris.fr>
2022-05-11 19:02:09 +0200

    substr to fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: kdhrif <kdhrif@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 12:19:17 by kdhrif            #+#    #+#             */
-/*   Updated: 2022/04/29 12:19:24 by kdhrif           ###   ########.fr       */
+/*   Updated: 2022/05/11 18:59:42 by kdhrif           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,7 +15,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
-	if (!dest && !src)
+	if (!dest || !src)
 		return (0);
 	i = 0;
 	while (i < n)
--- ft_substr.c
+++ ft_substr.c
@@ -2,19 +2,16 @@
       
   char *ft_substr(char const *s, unsigned int start, size_t len)    
   {    
-          char *pstart;          
           char *ptr;          
-          size_t i;    
-      
-          ptr = malloc(sizeof(char) * len + 1);    
-          if (!ptr)    
-                  return (NULL);    
-      
-          pstart = ft_strchr(s, start);     
-          if (!pstart)    
-                  return (NULL);    
-		
-	  ptr = ft_memcpy(ptr, pstart, len);
+
+	  if (!s)
+		  return (NULL);
+	  ptr = malloc(sizeof(char) * len + 1);    
+	  if (!ptr)    
+		  return (NULL);    
+
+	  ptr = ft_memcpy(ptr, &s[start], len);
 	  ptr[len] = '\0';
 	  return (ptr);
   }
+
