921a9e2bfa4acb0e8cc7037816ca0e455439f278
Karim DHRIF <kdhrif@e3r11p22.clusters.42paris.fr>
2022-05-11 18:35:33 +0200

    fixing substr

--- ft_substr.c
+++ ft_substr.c
@@ -1,20 +1,99 @@
-  #include "libft.h"    
-      
-  char *ft_substr(char const *s, unsigned int start, size_t len)    
-  {    
-          char *pstart;          
-          char *ptr;          
-          size_t i;    
-      
-          ptr = malloc(sizeof(char) * len + 1);    
-          if (!ptr)    
-                  return (NULL);    
-      
-          pstart = ft_strchr(s, start);     
-          if (!pstart)    
-                  return (NULL);    
-		
-	  ptr = ft_memcpy(ptr, pstart, len);
-	  ptr[len] = '\0';
-	  return (ptr);
-  }
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdhrif <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 18:14:47 by kdhrif            #+#    #+#             */
+/*   Updated: 2022/05/11 18:35:19 by kdhrif           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+
+	ptr = malloc(sizeof(char) * len + 1);
+	if (!ptr)
+		return (NULL);
+	ptr = ft_memcpy(ptr, &s[start], len);
+	ptr[len] = '\0';
+	return (ptr);
+}
+void    ft_print_result(char const *s)
+{                           
+	int             len;
+
+	len = 0;      
+	while (s[len])
+		len++;   
+	write(1, s, len);
+}
+
+int             main(int argc, const char *argv[])
+{
+	char    str[] = "lorem ipsum dolor sit amet";
+	char    *strsub;
+	int             arg;
+
+	alarm(5);
+	if (argc == 1)
+		return (0);
+	else if ((arg = atoi(argv[1])) == 1)
+	{
+		if (!(strsub = ft_substr(str, 0, 10)))
+			ft_print_result("NULL");
+		else
+			ft_print_result(strsub);
+		if (str == strsub)
+			ft_print_result("\nA new string was not returned");
+	}
+	else if (arg == 2)
+	{
+		if (!(strsub = ft_substr(str, 7, 10)))
+			ft_print_result("NULL");
+		else
+			ft_print_result(strsub);
+		if (str == strsub)
+			ft_print_result("\nA new string was not returned");
+	}
+	else if (arg == 3)
+	{
+		if (!(strsub = ft_substr(str, 7, 0)))
+			ft_print_result("NULL");
+		else
+			ft_print_result(strsub);
+		if (str == strsub)
+			ft_print_result("\nA new string was not returned");
+	}
+	{
+		if (!(strsub = ft_substr(str, 0, 0)))
+			ft_print_result("NULL");
+		else
+			ft_print_result(strsub);
+		if (str == strsub)
+			ft_print_result("\nA new string was not returned");
+	}
+	else if (arg == 5)
+	{
+		char *bullshit;
+		if (!(strsub = ft_substr(str, 400, 20)))
+			ft_print_result("NULL");
+		else
+		{
+			bullshit = (char *)&strsub[30];
+			bullshit = "FULL BULLSHIT";
+			if (strsub)
+				ft_print_result(strsub);
+			else
+				ft_print_result("rip");
+		}
+		if (str == strsub)
+			ft_print_result("\nA new string was not returned");
+		(void)bullshit;
+	}
+	return (0);
+}
