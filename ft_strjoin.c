301796b985a4b7881f66ea11fda2bb7d7a08c800
Karim DHRIF <kdhrif@e3r11p22.clusters.42paris.fr>
2022-05-12 14:51:54 +0200

    strjoin substr

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,17 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdhrif <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 14:00:02 by kdhrif            #+#    #+#             */
+/*   Updated: 2022/05/12 14:00:05 by kdhrif           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t i;
-	size_t j;
-	char *ptr;
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	char	*ptr;
 
 	i = ft_strlen(s1);
 	j = ft_strlen(s2);
-	ptr = malloc(sizeof(char) * (i+j));
+	ptr = malloc(sizeof(char) * (i + j + 1));
 	if (!ptr)
 		return (NULL);
-	ptr = ft_memcpy(ptr, s1, i);
-	ft_strlcat(ptr, s2, (i+j));	
+	k = 0;
+	while (k < i)
+	{
+		ptr[k] = s1[k];
+		k++;
+	}
+	k = 0;
+	while (k < j)
+	{
+		ptr[i + k] = s2[k];
+		k++;
+	}
+	ptr[i + j] = '\0';
 	return (ptr);
 }
