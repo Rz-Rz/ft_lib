8cdc688b6365a500255ed50c81bd9a2d74badf64
Karim DHRIF <kdhrif@e1r11p13.clusters.42paris.fr>
2022-05-13 13:08:02 +0200

    finished trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,16 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kdhrif <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/12 15:15:06 by kdhrif            #+#    #+#             */
-/*   Updated: 2022/05/12 16:39:13 by kdhrif           ###   ########.fr       */
+/*   Created: 2022/05/13 13:00:19 by kdhrif            #+#    #+#             */
+/*   Updated: 2022/05/13 13:00:23 by kdhrif           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_incharset(char* set, char c)
+int	ft_incharset(const char *set, char c)
 {
-	int i;
+	int	i;
 
 	i = -1;
 	while (set[++i])
@@ -23,34 +23,31 @@ int ft_incharset(char* set, char c)
 	return (0);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	int i;
-	int j;
-	int k;
-	char *str;
-	
+	int		i;
+	int		j;
+	int		k;
+	char	*str;
 
 	i = 0;
 	j = 0;
-	while (ft_incharset(s[i]))
+	while (ft_incharset(set, s1[i]))
 		i++;
-	if (s[i] == '\0')
+	if (s1[i] == '\0')
 		return (ft_strdup(""));
 	j = ft_strlen(s1) - 1;
-	while (ft_incharset(s[j]))
-		j--;
-	str = malloc(sizeof(char) * (j - i + 2))
+	while (ft_incharset(set, s1[j]))
+		--j;
+	str = malloc(sizeof(char) * (j - i + 2));
 	if (!str)
 		return (NULL);
 	k = 0;
-	while (k < (j - i) + 2)
+	while (k < (j - i + 1))
 	{
-		str[k] = s[i + k];
+		str[k] = s1[i + k];
 		k++;
 	}
 	str[k] = '\0';
 	return (str);
-
-
 }
